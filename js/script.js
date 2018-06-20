ef94c53d8698a082ce6ed53ef1b469d67993a5fa
james-whitehead <jameswhthead@gmail.com>
2018-06-20 17:05:36 +0100

    Added north/south boundaries to map panning

--- js/script.js
+++ js/script.js
@@ -3,26 +3,34 @@
  */
 function loadMap() {
     let map = L.map('id-map', {
+        maxBounds: [[-5600, -5600], [5600, 5600]],
         maxBoundsViscosity: 1.0
-    }).setView([54, -1.5], 4);
+    }).setView([54, -1.5], 3);
+
     let mapMaxZoom = 12;
-    let mapMinZoom = 2;
+    let mapMinZoom = 3;
+    let mapBounds = [[-8576 / 2, -8576 / 2], [8576 / 2, 8576 / 2]];
+
     // Background map layer
     let stamenWatercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}{r}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: 'abcd',
         maxZoom: mapMaxZoom,
         minZoom: mapMinZoom,
-        ext: 'png'
+        ext: 'png',
+        bounds: mapBounds
     });
+
     // Borders and cities layer
     let stamenLabels = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}{r}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: 'abcd',
         maxZoom: mapMaxZoom,
         minZoom: mapMinZoom,
-        ext: 'png'
+        ext: 'png',
+        bounds: mapBounds
     });
+
     stamenWatercolor.addTo(map);
     stamenLabels.addTo(map);
 }
