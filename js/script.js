bcfa0bf0ef5fb13ff5438ed2d1be5fc5398b010a
james-whitehead <jameswhthead@gmail.com>
2018-06-22 11:54:38 +0100

    Working lat/long resolve to address on map click

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function loadMap() {
     let map = L.map('id-map', {
         maxBounds: [[-5600, -5600], [5600, 5600]],
         maxBoundsViscosity: 1.0
-    }).setView([54, -1.5], 3);
+    }).setView([54, -1.5], 4);
 
     let mapMaxZoom = 12;
     let mapMinZoom = 3;
@@ -13,7 +13,9 @@ function loadMap() {
 
     // Background map layer
     let stamenWatercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}{r}.{ext}', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ' +
+            '<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; ' +
+            'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: 'abcd',
         maxZoom: mapMaxZoom,
         minZoom: mapMinZoom,
@@ -23,7 +25,9 @@ function loadMap() {
 
     // Borders and cities layer
     let stamenLabels = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}{r}.{ext}', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ' +
+            '<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; ' +
+            'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: 'abcd',
         maxZoom: mapMaxZoom,
         minZoom: mapMinZoom,
@@ -36,10 +40,43 @@ function loadMap() {
     map.on('click', onMapClick);
 }
 
+/**
+ * Listener function taking an event object, performs an OSM reverse lookup
+ * on the latitude and longitude returned from a click event.
+ * @param {object} event An event object that fires when the map is clicked on
+ */
 function onMapClick(event) {
-    console.log(event.latlng.wrap());
-    // TODO: lookup with lat/long
-    // Example JSON search: https://nominatim.openstreetmap.org/reverse?format=json&lat=54.77639823271888&lon=-2.7603149414062504&addressdetails=1
+    console.log(window.navigator.userAgent);
+    let latlng = event.latlng.wrap();
+    // Reverse address lookup from latlng
+    let url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latlng.lat}&lon=${latlng.lng}&addressdetails=1`
+    fetch(url, {
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'user-agent': window.navigator.userAgent,
+            'content-type': 'application/json'
+        },
+        method: 'POST',
+        mode: 'cors',
+        redirect: 'follow',
+        referrer: 'no-referrer'
+    })
+    .then(response => response.json())
+    .then((output) => {
+        searchNews(output);
+    })
+    .catch(err => {
+        throw err
+    });
+}
+
+/**
+ * Returns a Google News RSS feed for a query of this address
+ * @param {json} address JSON-formatted address information
+ */
+function searchNews(address) {
+    console.log(address.address);
 }
 
 window.onload = loadMap;
